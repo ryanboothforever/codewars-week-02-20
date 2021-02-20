1d5c631dab9b6181b9113ec09fdbfe592887edc0
Ryan Booth <ryanboothforever@gmail.com>
2021-02-20 08:00:34 -0600

    Added more kyus

--- main.js
+++ main.js
@@ -35,3 +35,66 @@ function getAverage(marks){
 return Math.floor(sum/marks.length);
 }
 
+//Given an array of integers. Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. If the input array is empty or null, return an empty array.
+
+function countPositivesSumNegatives(input){
+    if(!input || input.length == 0){
+        return [];
+    } else {
+    let final =[];
+    function posOnly(input){
+        let i = 0;
+        let n = 0;
+       input.forEach(element => {
+           element>0 ? i++ : n++;
+       });
+        return i
+    }
+    function negOnly(input){
+        let neg = input.filter(item=>item<0)
+        neg = neg.reduce((passedIn, item)=>{
+            return passedIn+item
+        })
+        return neg
+    }
+
+    final.push(posOnly(input))
+    final.push(negOnly(input))
+    return final
+}
+}
+
+// Kyu 7: Move every letter in the provided string forward 10 letters through the alphabet. If it goes past 'z', start again at 'a'. Input will be a string with length > 0.
+
+function moveTen(s){
+    function letterToNum(s){
+    let codesPlusTen=[]; 
+//seperate every string letter into seperate array parts
+  s=s.split("")
+  //Loop thru each letter in this new array and add 10 to each character count
+  let codes = s.map(letter=>letter.charCodeAt(0))
+codes.forEach(char => {
+  if(char === 122){
+        codesPlusTen.push(106)
+    }
+  else if(char<=112){
+    codesPlusTen.push(char+10)
+  }else if(char>=113 && char<122) {
+      let x = 122 - char;
+      let numRemianing = -(x - 10);
+      let newChar = numRemianing + 96;
+      codesPlusTen.push(newChar)
+    }
+})
+return codesPlusTen
+}
+newCodes= letterToNum(s)
+let letters = newCodes.map(num=> String.fromCharCode(num))
+return letters.join("")
+}
+//Way shorter way of doing it:
+function moveTen(s){
+  return s.split('').map(e => e.charCodeAt(0) + 10 > 122 
+                                ? String.fromCharCode(e.charCodeAt(0) - 16) 
+                                : String.fromCharCode(e.charCodeAt(0) + 10)).join('');
+}
